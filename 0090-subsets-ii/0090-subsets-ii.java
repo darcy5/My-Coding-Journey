074adb767bd4e9d7c9dec999a34ca2b8a58645b6
Debopama Basu <46404160+darcy5@users.noreply.github.com>
2022-11-07 17:35:31 +0530

    Time: 18 ms (6.51%), Space: 45.6 MB (6.24%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        HashSet<String> set = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<(1<<n);i++){
+            String setVal = "";
+            List<Integer> sub = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if(((i>>j)&1)!=0){
+                sub.add(nums[j]);
+                setVal += nums[j] + "^";
+                } 
+            }
+            if(set.add(setVal)) list.add(sub);
+        }
+        return list;
+    }
+}
