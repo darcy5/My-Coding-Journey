ee73970ed412bd178be0858e815c66e024a386a1
Debopama Basu <46404160+darcy5@users.noreply.github.com>
2025-01-12 01:20:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (69.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -10,8 +10,13 @@ public:
 
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            else if(nums[mid]>nums[mid-1]) low=mid+1;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+
+            if(nums[mid]>nums[mid-1]){
+                low=mid+1;
+            }
             else high=mid-1;
         }
         return -1;
