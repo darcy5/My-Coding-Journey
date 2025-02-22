fe2abe4a53e313b99fa252f6a5cb9a1b56eb6b26
Debopama Basu <46404160+darcy5@users.noreply.github.com>
2025-02-22 16:58:46 +0530

    Time: 23 ms (98.57%), Space: 106 MB (30.66%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,49 +1,24 @@
 class Solution {
-
-    int bSearch(int target_idx,int k,vector<int>& nums, vector<long>& prefixSum){
-
-        int target = nums[target_idx];
-
-        int left = 0;
-
-        int right = target_idx;
-
-        int best_idx = target_idx;
-
-        while(left <= right){
-            int mid = left+(right-left)/2;
-            long count = target_idx-mid+1;
-            long windowSum = count*target;
-            long currSum = prefixSum[target_idx]-prefixSum[mid]+nums[mid];
-
-            int ops = windowSum-currSum;
-
-            if(ops > k){
-                left = mid+1;
-            }
-            else {
-                best_idx = mid;
-                right = mid-1;
-            }
-        }
-        return target_idx - best_idx + 1;
-    }
 public:
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
-
-        sort(begin(nums),end(nums));
-
-        vector<long> prefixSum(n);
-        prefixSum[0] = nums[0];
-        for (int i=1; i<n; i++){
-            prefixSum[i] = prefixSum[i-1]+nums[i];
-        }
-
+        sort (begin(nums), end(nums));
+        int l = 0; 
         int result = 0;
-        for(int target_idx=0; target_idx<n; target_idx++){
-            result = max(result, bSearch(target_idx,k,nums,prefixSum));
+        long currSum = 0;
+         
+        for ( int r = 0 ; r < n; r++){
+            long target = nums[r];
+            currSum += nums[r];
+
+            while ((r-l+1)* target - currSum > k ){
+                currSum -= nums[l];
+                l++;
+            }
+
+            result = max( result, r-l+1);
         }
-        return result; 
+
+        return result;
     }
 };
