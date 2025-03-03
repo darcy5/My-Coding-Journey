53f95e13b6bd413e313cdd7809236281ec07c47d
Debopama Basu <46404160+darcy5@users.noreply.github.com>
2025-03-03 23:21:58 +0530

    Time: 0 ms (100%), Space: 13.9 MB (91.37%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,25 @@
 class Solution {
-    
-public:
-    void findCombinations(int ind,int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-        if(ind==arr.size()){
-            if(target==0){
+    void findComb( int ind, int target, vector<int>& arr, vector<vector<int>> &ans, vector<int> &ds){
+        if ( ind == arr.size()){
+            if (target == 0){
                 ans.push_back(ds);
             }
             return;
         }
-        //pick up the element
-        if(arr[ind]<=target){
+
+        if ( arr[ind]<=target){
             ds.push_back(arr[ind]);
-            findCombinations(ind,target-arr[ind],arr,ans,ds);
+            findComb(ind, target-arr[ind], arr, ans, ds);
             ds.pop_back();
         }
-        findCombinations(ind+1,target,arr,ans,ds);
+        findComb(ind+1,target, arr, ans, ds);
     }
-    
+
 public:
-    //given a function of vector of vectors
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        //first declare a vector of vector to store ans
         vector<vector<int>> ans;
-        vector<int> ds; //used in recursion to find combinations
-        findCombinations(0,target,candidates,ans,ds);
+        vector <int> ds;
+        findComb(0, target, candidates, ans, ds);
         return ans;
     }
 };
